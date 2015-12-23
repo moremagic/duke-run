42912046f8088c606776080ad8219fb2c1fb51fd
moremagic <itoumagic@gmail.com>
2015-12-23 22:12:50 +0900

    空を飛べるように修正。またＤ＆Ｄでの移動にも対応

--- src/duke/run/DukeFrame2.java
+++ src/duke/run/DukeFrame2.java
@@ -10,12 +10,11 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.Window;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -25,29 +24,29 @@ import javax.swing.SwingUtilities;
  * @author mitsu
  */
 public class DukeFrame2 extends JFrame {
-
+    
+    private static final ImageIcon image_fly = new ImageIcon(DukeFrame2.class.getResource("/duke/run/resouces/duke.png"));
     private static final ImageIcon image1 = new ImageIcon(DukeFrame2.class.getResource("/duke/run/resouces/run1.gif"));
     private static final ImageIcon image2 = new ImageIcon(DukeFrame2.class.getResource("/duke/run/resouces/run2.gif"));
     private boolean imageFlg = true;
     private boolean stopFlg = true;
     private boolean turnFlg = false;
-
+    
     public DukeFrame2() {
         initComponents();
+        actionThread.start();
     }
-
+    
     public void setStopFlg(boolean stopFlg) {
         this.stopFlg = stopFlg;
-        if (!this.stopFlg) {
-            actionThread.start();
-        }
-
+        repaint();
     }
-
+    
     public void setTurnFlg(boolean turnFlg) {
         this.turnFlg = turnFlg;
+        repaint();
     }
-
+    
     private void initComponents() {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -55,53 +54,60 @@ public class DukeFrame2 extends JFrame {
         setResizable(false);
         setAlwaysOnTop(true);
         setMinimumSize(new Dimension(image1.getIconWidth(), image1.getIconHeight()));
-
-        new Thread(new Runnable() {
-            public void run() {
-                while (!isVisible()) {
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException ex) {
-                    }
-                }
-                while (isVisible()) {
-                    if (!stopFlg) {
-                        imageFlg = !imageFlg;
-                    }
-
-                    SwingUtilities.invokeLater(new Runnable() {
-                        public void run() {
-                            repaint();
-                        }
-                    });
-
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException ex) {
-                    }
-                }
-            }
-        }).start();
+        
+        DragWindowListener dwl = new DragWindowListener();
+        addMouseListener(dwl);
+        addMouseMotionListener(dwl);
     }
-
+    
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-
+        
+        java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
+        java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
+        if (desktopBounds.getHeight() > (getLocation().y + getHeight())) {
+            paintFly(g);
+        } else {
+            paintRun(g);
+        }
+        
+    }
+    
+    public void paintRun(Graphics g) {
+        if (!stopFlg) {
+            imageFlg = !imageFlg;
+        }
+        
         Image img;
         if (imageFlg) {
             img = image1.getImage();
         } else {
             img = image2.getImage();
         }
-
+        
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform attr = AffineTransform.getTranslateInstance(1.0d, 1.0d);
         if (turnFlg) {
             attr = AffineTransform.getScaleInstance(-1.0d, 1.0d);
             attr.translate(-img.getWidth(this), 0);
         }
-
+        
+        g2d.drawImage(img, attr, this);
+    }
+    
+    public void paintFly(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        AffineTransform attr = AffineTransform.getScaleInstance(0.35d, 0.35d);
+        
+        Image img;
+        img = image_fly.getImage();
+        
+        if (turnFlg) {
+            attr = AffineTransform.getScaleInstance(-0.35d, 0.35d);
+            attr.translate(-img.getWidth(this), 0);
+        }
+        
         g2d.drawImage(img, attr, this);
     }
 
@@ -109,77 +115,60 @@ public class DukeFrame2 extends JFrame {
     private final Thread actionThread = new Thread(new Runnable() {
         @Override
         public void run() {
+            dropDuke().run();
             while (isVisible()) {
-                createRightRun(30).run();
+                dropDuke().run();
+                createMoveRun(30, 5, 0).run();
+                waitDuke(3).run();
+                createMoveRun(30, 8, -10).run();
                 waitDuke(5).run();
-                createRightRun(30).run();
-                waitDuke(5).run();
-                createLeftRun(30).run();
-                waitDuke(5).run();
-                createLeftRun(30).run();
+                createMoveRun(30, -10, 0).run();
+                waitDuke(2).run();
+                createMoveRun(30, -3, -20).run();
                 waitDuke(5).run();
             }
         }
     });
-
-    private Runnable createRightRun(int move) {
+    
+    private Runnable createMoveRun(int move, int dx, int dy) {
         return () -> {
             java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
             java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
-            
+            if (desktopBounds.getHeight() > (getLocation().y + getHeight())) {
+                dropDuke().run();
+            }
+
             //右側に
             java.awt.EventQueue.invokeLater(() -> {
                 setStopFlg(false);
             });
             java.awt.EventQueue.invokeLater(() -> {
-                setTurnFlg(false);
+                setTurnFlg(dx < 0);
             });
             for (int i = 0; i < move; i++) {
                 java.awt.EventQueue.invokeLater(() -> {
-                    setLocation(getLocation().x + 5, getLocation().y);
+                    setLocation(getLocation().x + dx, getLocation().y + dy);
                 });
                 try {
                     Thread.sleep(50); //Sleepする
                 } catch (InterruptedException e) {
                 }
                 
-                if (desktopBounds.getWidth() <= getLocation().x || Thread.currentThread().isInterrupted()) {
+                if (desktopBounds.getWidth() <= getLocation().x || 0 >= getLocation().x) {
                     break;
                 }
             }
         };
     }
-
-    private Runnable createLeftRun(int move) {
+    
+    private Runnable waitDuke(int cnt) {
         return () -> {
             java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
             java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
-            
-            //左側に
-            java.awt.EventQueue.invokeLater(() -> {
-                setStopFlg(false);
-            });
-            java.awt.EventQueue.invokeLater(() -> {
-                setTurnFlg(true);
-            });
-            for (int i = 0; i < move; i++) {
-                java.awt.EventQueue.invokeLater(() -> {
-                    setLocation(getLocation().x - 5, getLocation().y);
-                });
-                try {
-                    Thread.sleep(50); //300ミリ秒Sleepする
-                } catch (InterruptedException e) {
-                }
-                
-                if (0 >= getLocation().x) {
-                    break;
-                }
+            if (desktopBounds.getHeight() > (getLocation().y + getHeight())) {
+                return;
             }
-        };
-    }
-
-    private Runnable waitDuke(int cnt) {
-        return () -> {
+            
             int exitCnt = cnt;
             boolean bTurn = true;
             java.awt.EventQueue.invokeLater(() -> {
@@ -191,12 +180,67 @@ public class DukeFrame2 extends JFrame {
                     setTurnFlg(b);
                 });
                 try {
-                    Thread.sleep(500); //Sleepする
+                    Thread.sleep(1000); //Sleepする
                     bTurn = !bTurn;
                 } catch (InterruptedException e) {
                 }
             }
         };
     }
+    
+    private Runnable dropDuke() {
+        return () -> {
+            java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
+            java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
 
+            //動き止める
+            java.awt.EventQueue.invokeLater(() -> {
+                setStopFlg(true);
+            });
+            //落下させる
+            for (int i = 0; i < 1000; i++) {
+                java.awt.EventQueue.invokeLater(() -> {
+                    setLocation(getLocation().x, getLocation().y + 5);
+                });
+                try {
+                    Thread.sleep(50); //Sleepする
+                } catch (InterruptedException e) {
+                }
+                
+                if (desktopBounds.getHeight() <= (getLocation().y + getHeight())) {
+                    break;
+                }
+            }
+            //動き始める
+            java.awt.EventQueue.invokeLater(() -> {
+                setStopFlg(false);
+            });
+        };
+    }
+    
+}
+
+class DragWindowListener extends MouseAdapter {
+    
+    private final Point startPt = new Point();
+    private Window window;
+    
+    @Override
+    public void mousePressed(MouseEvent me) {
+        startPt.setLocation(me.getPoint());
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent me) {
+        if (window == null) {
+            if (me.getComponent() instanceof Window) {
+                window = (Window) me.getComponent();
+            } else {
+                window = SwingUtilities.windowForComponent(me.getComponent());
+            }
+        }
+        Point eventLocationOnScreen = me.getLocationOnScreen();
+        window.setLocation(eventLocationOnScreen.x - startPt.x,
+                eventLocationOnScreen.y - startPt.y);
+    }
 }
--- src/duke/run/DukeRun.java
+++ src/duke/run/DukeRun.java
@@ -5,8 +5,6 @@
  */
 package duke.run;
 
-import javax.swing.JFrame;
-
 /**
  *
  * @author mitsu
@@ -17,81 +15,9 @@ public class DukeRun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
-        java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
-
         DukeFrame2 frame = new DukeFrame2();
         java.awt.EventQueue.invokeLater(() -> {
             frame.setVisible(true);
         });
-
-        //落下させる
-        for (int i = 0; i < 1000; i++) {
-            final int j = i;
-            java.awt.EventQueue.invokeLater(() -> {
-                frame.setLocation(0, j * 5);
-            });
-            try {
-                Thread.sleep(50); //300ミリ秒Sleepする
-            } catch (InterruptedException e) {
-            }
-
-            if (desktopBounds.getHeight() <= (frame.getLocation().y + frame.getHeight())) {
-                break;
-            }
-        }
-
-        //動き始める
-        java.awt.EventQueue.invokeLater(() -> {
-            frame.setStopFlg(false);
-        });
-
-
-//        //右側に
-//        for (int i = 0; i < 1000; i++) {
-//            final int j = i;
-//            java.awt.EventQueue.invokeLater(() -> {
-//                frame.setLocation(frame.getLocation().x + 5, frame.getLocation().y);
-//            });
-//            try {
-//                Thread.sleep(50); //300ミリ秒Sleepする
-//            } catch (InterruptedException e) {
-//            }
-//
-//            if (desktopBounds.getWidth() <= frame.getLocation().x) {
-//                break;
-//            }
-//        }
-//
-//        //ターン
-//        java.awt.EventQueue.invokeLater(() -> {
-//            frame.setTurnFlg(true);
-//        });
-//        
-//        //左側に
-//        for (int i = 0; i < 1000; i++) {
-//            final int j = i;
-//            java.awt.EventQueue.invokeLater(() -> {
-//                frame.setLocation(frame.getLocation().x - 5, frame.getLocation().y);
-//            });
-//            try {
-//                Thread.sleep(50); //300ミリ秒Sleepする
-//            } catch (InterruptedException e) {
-//            }
-//
-//            if (0 >= frame.getLocation().x) {
-//                break;
-//            }
-//        }
-//
-//        //終了
-//        System.out.println("exit");
-//
-//        java.awt.EventQueue.invokeLater(() -> {
-//            frame.setVisible(false);
-//            frame.dispose();
-//        });
-        
-        //System.exit(0);
     }
 }
